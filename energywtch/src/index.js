2e8e72fa13b83f6dddec5782982546a502705eef
aaryans2017@gmail.com <aaryans2017@gmail.com>
2026-04-18 17:13:08 -0400

    all changes

---
+++ energywtch/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import EnergyWatch from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <EnergyWatch />
+  </React.StrictMode>
+);
